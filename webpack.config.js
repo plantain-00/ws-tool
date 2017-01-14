1cd3f8e514b108e403461b49d1fb2ae9183e93ca
york yao <yaoao12306@outlook.com>
2017-01-14 08:40:12 +0800

    use vue.min.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     ],
     resolve: {
         alias: {
-            "vue$": "vue/dist/vue.js",
+            "vue$": "vue/dist/vue.min.js",
             "protobufjs": "protobufjs/dist/protobuf.min.js"
         }
     }
