aa5cfc50b3fd028e3280a8da4e54f2206f7861fa
york yao <yaoao12306@outlook.com>
2017-05-27 18:59:27 +0800

    remove redundant declaration and add issue/pull request template

--- index.ts
+++ index.ts
@@ -1132,21 +1132,7 @@ proxyWebSocket.onmessage = event => {
     app.onmessage(event);
 };
 
-declare class ServiceWorkerRegistration {
-    installing: boolean;
-    waiting: boolean;
-    active: boolean;
-}
-
-declare class ServiceWorker {
-    register(scriptUrl: string, options?: { scope?: string }): Promise<ServiceWorkerRegistration>;
-}
-
-declare const navigator: {
-    serviceWorker: ServiceWorker;
-};
-
-if ("serviceWorker" in navigator) {
+if (navigator.serviceWorker) {
     navigator.serviceWorker.register("service-worker.bundle.js").catch(error => {
         console.log("registration failed with error: " + error);
     });
