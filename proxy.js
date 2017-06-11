2cbbf3c78edcf3b4cf377cb1614e2224a9abc89f
york yao <yaoao12306@outlook.com>
2017-06-11 13:59:15 +0800

    add demo pages in master branch

---
+++ proxy.js
@@ -0,0 +1,111 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var uws_1 = require("uws");
+var http = require("http");
+var net = require("net");
+var dgram = require("dgram");
+var node_fetch_1 = require("node-fetch");
+var compression = require("compression");
+var server = http.createServer();
+var wss = new uws_1.Server({ server: server });
+var app = express();
+app.use("/", express.static(__dirname));
+app.disable("x-powered-by");
+app.use(compression());
+var toUrlHeaderName = "x-to-url";
+var headersName = "x-headers";
+app.all("/proxy", function (request, proxyResponse) {
+    var url = request.header(toUrlHeaderName);
+    if (!url) {
+        proxyResponse.send("No header: " + toUrlHeaderName);
+    }
+    else {
+        var chunks_1 = [];
+        request.on("data", function (chunk) {
+            chunks_1.push(chunk);
+        });
+        request.on("end", function () {
+            var headerArray = JSON.parse(request.header(headersName));
+            var headers = {};
+            for (var _i = 0, headerArray_1 = headerArray; _i < headerArray_1.length; _i++) {
+                var header = headerArray_1[_i];
+                headers[header.key] = header.value;
+            }
+            node_fetch_1.default(url, {
+                headers: headers,
+                body: Buffer.concat(chunks_1),
+                method: request.method,
+                compress: false,
+            }).then(function (response) {
+                response.headers.forEach(function (value, name) {
+                    proxyResponse.setHeader(name, value);
+                });
+                response.buffer().then(function (buffer) {
+                    proxyResponse.status(response.status).send(buffer);
+                }, function (error) {
+                    proxyResponse.status(response.status).send(error.message);
+                });
+            }, function (error) {
+                proxyResponse.send(error.message);
+            });
+        });
+    }
+});
+wss.on("connection", function (ws) {
+    var tcpClient;
+    var udpClient = dgram.createSocket("udp4");
+    udpClient.on("message", function (msg, rinfo) {
+        ws.send(msg);
+    });
+    ws.on("message", function (data) {
+        var protocol = JSON.parse(data);
+        if (protocol.kind === "tcp:connect") {
+            if (tcpClient) {
+                tcpClient.destroy();
+            }
+            tcpClient = net.connect(protocol.port, protocol.host, function () {
+                var responseProtocol = {
+                    kind: "tcp:connected",
+                };
+                ws.send(JSON.stringify(responseProtocol));
+            });
+            tcpClient.on("close", function (hadError) {
+                var responseProtocol = {
+                    kind: "tcp:disconnected",
+                };
+                ws.send(JSON.stringify(responseProtocol));
+            });
+            tcpClient.on("error", function (error) {
+                ws.send("errored: " + error.stack);
+            });
+            tcpClient.on("timeout", function () {
+                ws.send("timeout");
+            });
+            tcpClient.on("data", function (tcpData) {
+                ws.send(tcpData, { binary: true });
+            });
+        }
+        else if (protocol.kind === "tcp:disconnect") {
+            if (tcpClient) {
+                tcpClient.destroy();
+            }
+        }
+        else if (protocol.kind === "tcp:send") {
+            if (tcpClient) {
+                var message = protocol.isBinary ? new Buffer(protocol.message.split(",")) : protocol.message;
+                tcpClient.write(message);
+            }
+        }
+        else if (protocol.kind === "udp:send") {
+            var message = protocol.isBinary ? new Buffer(protocol.message.split(",")) : protocol.message;
+            udpClient.send(message, protocol.port, protocol.address);
+        }
+    });
+});
+server.on("request", app);
+var port = 12345;
+server.listen(port, function () {
+    // tslint:disable-next-line:no-console
+    console.log("The proxy is running, and listening: " + port);
+});
