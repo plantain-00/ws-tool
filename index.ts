386eb83a9ffa550ffab74b30c5ee78f8d7ea7947
york yao <yaoao12306@outlook.com>
2017-06-19 19:12:03 +0800

    better import

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as Vue from "vue";
+import Vue from "vue";
 import Component from "vue-class-component";
 import { Decoder } from "socket.io-parser";
 import * as Clipboard from "clipboard";
