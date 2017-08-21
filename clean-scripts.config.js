461ad39c83b20b6e30d4ebf5610613c9b481e29f
york yao <yaoao12306@outlook.com>
2017-08-21 10:44:09 +0800

    improve watch script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,17 +6,19 @@ module.exports = {
       js: [
         `file2variable-cli app.template.html -o variables.ts --html-minify`,
         `tsc`,
-        `webpack --display-modules`
+        `webpack --display-modules --config webpack.config.js`
       ],
-      css: [
-        `cleancss ./node_modules/bootstrap/dist/css/bootstrap.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css -o vendor.bundle.css`,
-        `cleancss index.css -o index.bundle.css`
-      ],
-      clean: `rimraf vendor.bundle-*.js vendor.bundle-*.css index.bundle-*.js index.bundle-*.css`
+      css: {
+        vendor: `cleancss ./node_modules/bootstrap/dist/css/bootstrap.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css -o vendor.bundle.css`,
+        index: `cleancss index.css -o index.bundle.css`
+      },
+      clean: `rimraf vendor.bundle-*.js vendor.bundle-*.css index.bundle-*.js index.bundle-*.css`,
     },
     `rev-static`,
-    `sw-precache --config sw-precache.config.js`,
-    `uglifyjs service-worker.js -o service-worker.bundle.js`
+    [
+      `sw-precache --config sw-precache.config.js`,
+      `uglifyjs service-worker.js -o service-worker.bundle.js`
+    ]
   ],
   lint: {
     ts: `tslint "*.ts" "tests/*.ts"`,
@@ -45,5 +47,12 @@ module.exports = {
     js: `standard --fix "**/*.config.js"`
   },
   release: `clean-release`,
-  watch: `watch-then-execute "*.ts" "*.css" "*.template.html" --exclude "variables.ts" --script "npm run build"`
+  watch: {
+    template: `file2variable-cli app.template.html -o variables.ts --html-minify --watch`,
+    src: `tsc --watch`,
+    webpack: `webpack --watch`,
+    css: `watch-then-execute "index.css" --script "clean-scripts build[0].css.index"`,
+    rev: `rev-static --watch`,
+    sw: `watch-then-execute "vendor.bundle-*.js" "index.html" "worker.bundle.js" --script "clean-scripts build[2]"`
+  }
 }
