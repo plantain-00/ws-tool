3024e2a31964a11fa9e1623738871394e9273ed4
york yao <yaoao12306@outlook.com>
2017-08-21 11:18:36 +0800

    fix CI

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,13 +6,13 @@ module.exports = {
       js: [
         `file2variable-cli app.template.html -o variables.ts --html-minify`,
         `tsc`,
-        `webpack --display-modules --config webpack.config.js`
+        `webpack --display-modules`
       ],
       css: {
         vendor: `cleancss ./node_modules/bootstrap/dist/css/bootstrap.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css -o vendor.bundle.css`,
         index: `cleancss index.css -o index.bundle.css`
       },
-      clean: `rimraf vendor.bundle-*.js vendor.bundle-*.css index.bundle-*.js index.bundle-*.css`,
+      clean: `rimraf vendor.bundle-*.js vendor.bundle-*.css index.bundle-*.js index.bundle-*.css`
     },
     `rev-static`,
     [
