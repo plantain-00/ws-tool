e33887ef4ced9e7980b088598661e44318d63d41
york yao <yaoao12306@outlook.com>
2017-09-25 17:03:19 +0800

    fix script error

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -14,8 +14,8 @@ module.exports = {
       css: {
         vendor: `cleancss ./node_modules/bootstrap/dist/css/bootstrap.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css -o vendor.bundle.css`,
         index: [
-          `postcss index.css -o index.postss.css`,
-          `cleancss index.postss.css -o index.bundle.css`
+          `postcss index.css -o index.postcss.css`,
+          `cleancss index.postcss.css -o index.bundle.css`
         ]
       },
       clean: `rimraf vendor.bundle-*.js vendor.bundle-*.css index.bundle-*.js index.bundle-*.css`
