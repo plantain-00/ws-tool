9aa48f682d4c5777f89ffd4836faaa14a5c88d7b
york yao <yaoao12306@outlook.com>
2017-11-15 10:15:23 +0800

    update dependencies

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -6,7 +6,7 @@ import * as puppeteer from "puppeteer";
     await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" });
     await page.goto(`http://localhost:8000`);
     await page.waitFor(2000);
-    await page.screenshot({ path: `screenshots/initial.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/initial.png` });
 
     browser.close();
 })();
