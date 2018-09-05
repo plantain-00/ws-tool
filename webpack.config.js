9cea70f8643b41db518095e92800a16b8a8d724c
york yao <yaoao12306@outlook.com>
2018-09-05 08:52:27 +0800

    build: use ts-loader in watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: {
@@ -7,6 +9,14 @@ module.exports = {
     path: __dirname,
     filename: '[name].bundle.js'
   },
+  resolve: isDev ? {
+    extensions: ['.ts', '.tsx', '.js']
+  } : undefined,
+  module: isDev ? {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  } : undefined,
   optimization: {
     splitChunks: {
       cacheGroups: {
