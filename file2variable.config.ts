2d68d14f28a5fe7d38d5874f7cbb7c8dabecd154
York Yao <me@yorkyao.com>
2020-10-05 10:26:36 +0800

    feat: use vue3

--- file2variable.config.ts
+++ file2variable.config.ts
@@ -1,13 +1,15 @@
-export default {
+import { Configuration } from 'file2variable-cli'
+
+const config: Configuration = {
   files: [
     'app.template.html'
   ],
   handler: () => {
     return {
-      type: 'vue',
-      name: 'App',
-      path: './index'
+      type: 'vue3',
     }
   },
   out: 'variables.ts'
 }
+
+export default config
